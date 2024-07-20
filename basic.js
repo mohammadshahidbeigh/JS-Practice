29412e3bef86874e9a0326b1879eabc3cacc632c
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-07-20 23:50:24 +0530

    "Added a commented out line in calcAverage function and replaced it with a concise addition operator"

--- basic.js
+++ basic.js
@@ -899,6 +899,7 @@ GOOD LUCK �
 const calcAverage = (arr) => {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
+    //  sum = sum + arr[i];
     sum += arr[i];
   }
   return sum / arr.length;
