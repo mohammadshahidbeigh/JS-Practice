845042b19f43b90d1c2eb87d139a806fceb58113
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-07-20 18:29:27 +0530

    "Added object examples and coding challenges to basic.js"

--- basic.js
+++ basic.js
@@ -685,3 +685,114 @@ console.log(tips);
 console.log(total);
 
 // Objects
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasdrivingLicence: false,
+  birthYear: 2001,
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  calcAgeArrow: () => {
+    const currentYear = 2037;
+    // Using the outer 'this' context, which is not ideal in this scenario
+    return currentYear - jonas.birthYear;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.age}-year old ${
+      this.job
+    }, and he has ${this.hasdrivingLicence ? "a" : "no"} driver's license `;
+  },
+};
+console.log(jonas);
+console.log(jonas.lastName);
+// specail method
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+//others
+console.log(jonas["age"]);
+console.log(jonas["job"]);
+console.log(jonas["friends"]);
+console.log(jonas.friends.length);
+console.log(jonas.friends[0]);
+console.log(jonas.friends[2]);
+console.log(jonas["friends"][2]);
+console.log(jonas.hasdrivingLicence);
+console.log(jonas.calcAge());
+console.log(jonas.calcAgeArrow());
+console.log(jonas.age);
+console.log(jonas.getSummary());
+
+// const interestedIn = prompt(
+//   "What do you want to know about Jonas Choose between firstName, lastName, age, job, and friends"
+// );
+// console.log(jonas[interestedIn]);
+
+// Addong in Objects
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtmann";
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`
+);
+// "Jonas is a 36-year old teacher, and he has a driver's license"
+console.log(
+  `${jonas.firstName} is a ${jonas.age}-year old ${jonas.job}, and he has ${
+    jonas.hasdrivingLicence ? "a" : "no"
+  } driver's license`
+);
+/*
+Coding Challenge #3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall.
+GOOD LUCK �
+*/
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: "78",
+  height: "1.69",
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: "92",
+  height: "1.95",
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+console.log(mark.calcBMI(), john.calcBMI());
+
+mark.calcBMI() > john.calcBMI()
+  ? console.log(
+      `"Mark's BMI ${mark.calcBMI()} is higher than John's ${john.calcBMI()}`
+    )
+  : console.log(
+      `"John's BMI ${john.calcBMI()} is higher than Mark's ${mark.calcBMI()}`
+    );
