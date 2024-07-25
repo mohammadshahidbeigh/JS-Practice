50a7a7af6b0f720395e69fee9d6c164268897650
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-07-25 18:02:48 +0530

    "Added examples for for-of loop, enhanced object literals, optional chaining, and looping objects"

--- modern.js
+++ modern.js
@@ -393,7 +393,8 @@ team1 < team2 && console.log('Team 1 is more likely to win'); // Short Circuitin
 team1 > team2 && console.log('Team 2 is more likely to win');
 team1 === team2 && console.log('Draw');
 
-// 7. The for-of Loop
+// 7. The for-of Loop // Modern way for using a for loop
+
 const menu2 = [...restaurant.starterMenu, ...restaurant.mainMenu];
 for (const item of menu2) console.log(item);
 
@@ -403,4 +404,88 @@ for (const item of menu2) console.log(item);
 
 for (const [i, j] of menu2.entries()) {
   console.log(`${i + 1}: ${j}`);
+}
+
+// 8. Enhanced Object Literals
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours2 = {
+  [weekdays[3]]: {
+    // Destructuring with Enhanced Object Literals
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+// console.log(openingHours2);
+const restaurant2 = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours2, // Enhanced ES6 Object Literals
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+console.log(restaurant2);
+
+// 9. Optional Chaining (?.)
+// restaurant.openingHours.mon // Error
+// restaurant.openingHours.mon.open // Error
+// restaurant.openingHours.mon?.open // undefined
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Shahid', email: 'shahid@gmail.com' }];
+console.log(users[0]?.name ?? 'User array empty');
+console.log(users[1]?.name ?? 'User array empty');
+
+// 10. Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES - Key
+const properties = Object.keys(openingHours2);
+console.log(properties);
+// Property VALUES
+const values = Object.values(openingHours2);
+console.log(values);
+
+// Entire Object
+const entries = Object.entries(openingHours2);
+console.log(entries);
+
+// Loop over an Object
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key}, we open at ${open} and close at ${close}`);
 }
