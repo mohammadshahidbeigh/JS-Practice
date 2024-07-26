3efaa93cc3c51f6065d49beb55f7bfe7c1e0aa56
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-07-26 22:49:43 +0530

    "Added string manipulation examples and a function to check middle seat in airline booking"

--- modern.js
+++ modern.js
@@ -713,3 +713,36 @@ for (const [min, event] of gameEvents) {
 // console.log(gameEvents.get(64));
 
 // 13. Working with Strings - Part 1
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+console.log(airline.length);
+console.log('B737'.length);
+// console.log('B737'.charAt(0));
+// console.log('B737'.charAt(1));
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+  else console.log('You got lucky 😎');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+// 14. Working with Strings - Part 2
