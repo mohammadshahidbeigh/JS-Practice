0fb0320d87d7080cbef057e342ee380fcf09b5ea
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-07-28 21:56:52 +0530

    "Updated functions.js with new code for poll app and added CSS and HTML for poll button in index.html"

--- functions.js
+++ functions.js
@@ -354,8 +354,9 @@ const bookLH = book.bind(lufthansa);
 const bookLX = book.bind(swiss);
 
 bookEW(23, 'Steven Williams');
+bookLH(23, 'Steven Williams');
 
-const bookEW23 = book.bind(eurowings, 23);
+const bookEW23 = book.bind(eurowings, 23); // Partial application - setting a default value
 bookEW23('Jonas Schmedtmann');
 bookEW23('Martha Cooper');
 
@@ -374,6 +375,7 @@ lufthansa.buyPlane = function () {
 //   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
 // Partial application
+
 const addTax = (rate, value) => value + value * rate;
 console.log(addTax(0.1, 200));
 
@@ -383,6 +385,8 @@ const addVAT = addTax.bind(null, 0.23);
 console.log(addVAT(100));
 console.log(addVAT(23));
 
+// Old way with same result
+
 const addTaxRate = function (rate) {
   return function (value) {
     return value + value * rate;
@@ -391,3 +395,110 @@ const addTaxRate = function (rate) {
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
+
+/*
+Coding Challenge #1
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an
+array with the number of replies for each option. This data is stored in the starter
+'poll' object below.
+Your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The
+method does 2 things:
+1.1. Display a prompt window for the user to input the number of the
+selected option. The prompt should look like this:
+What is your favourite programming language?
+0: JavaScript
+1: Python
+2: Rust
+3: C++
+(Write option number)
+1.2. Based on the input number, update the 'answers' array property. For
+example, if the option is 3, increase the value at position 3 of the array by
+1. Make sure to check if the input is a number and if the number makes
+sense (e.g. answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The
+method takes a string as an input (called 'type'), which can be either 'string'
+or 'array'. If type is 'array', simply display the results array as it is, using
+console.log(). This should be the default option. If type is 'string', display a
+string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each
+'registerNewAnswer' method call.
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
+data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
+object! So what should the this keyword look like in this situation?
+The Complete JavaScript Course 21
+Test data for bonus:
+§ Data 1: [5, 2, 3]
+§ Data 2: [1, 5, 3, 9, 6, 1]
+Hints: Use many of the tools you learned about in this and the last section 😉
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // Register answer using Short Circuiting
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll)); // We use bind method bz this has only scope upto defined object
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string'); // Here we use call method in order to create a new array bz displayResults use this.answer which refers to answers array.
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
+
+// IIFE - Immediately Invoked Function Expressions
+
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+// This will never run again
+// IIFE
+(function () {
+  console.log('This will never run again');
+})();
+// This will never run again
+(() => console.log('This will ALSO never run again'))();
+// This will ALSO never run again
+// Why we use IIFE
+// 1. Data privacy
+// 2. Block scope
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
