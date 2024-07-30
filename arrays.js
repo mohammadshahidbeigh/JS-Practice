e1bf34a06934f68d4e51fe902607b9f7f288fd16
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-07-30 18:05:01 +0530

    "Added examples of using the reduce method on arrays, including calculating a balance and finding the maximum value."

--- arrays.js
+++ arrays.js
@@ -193,3 +193,24 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+
+// The reduce Method ------ Most useful array method
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0); // using a arrow function
+console.log(balance);
+
+let balance2 = 0; // With the old For of Loop
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
