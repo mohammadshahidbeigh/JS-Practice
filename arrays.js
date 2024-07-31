874da8d5dd4d12a566578523d2bc10fcfb3ba64c
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-07-31 12:03:29 +0530

    "Added new code examples for array methods, including chaining, find, some, every, flat, flatMap, and sorting, with console logs for testing and debugging."

--- arrays.js
+++ arrays.js
@@ -257,3 +257,121 @@ const calcAverageHumanAge = function (dogsJulia, dogsKate) {
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3], [16, 6, 10, 5, 6, 1, 4]);
 // calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3], [16, 6, 10, 5, 6, 1, 4]);
+
+// The Magic of Chaining Methods --------IMP
+const eurToUsd2 = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  //   .map((mov, i, arr) => {
+  //      console.log(arr);
+  //     return mov * eurToUsd;
+  //   }) // debugging during pipeline
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+
+// Coding Challenge #3
+/*
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this
+time as an arrow function, and using chaining!
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK 😀
+*/
+const calcAverageHumanAge2 = (dogsJulia, dogsKate) =>
+  dogsJulia
+    .concat(dogsKate)
+    .map(dog => (dog <= 2 ? 2 * dog : 16 + dog * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+console.log(
+  calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3], [16, 6, 10, 5, 6, 1, 4])
+);
+
+// The find Method
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+// console.log(accounts);
+
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+// console.log(account);
+
+// Some and Every Methods-(Condition Methods)
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+// console.log(movements.every(mov => mov > 0));
+// console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback--- Another way to write the above function
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
+
+// flat and flatMap --- Imp
+const arr5 = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr5.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8]; // nested array
+console.log(arrDeep.flat(2));
+
+// flat
+// const overalBalance = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance);
+
+// flatMap
+// const overalBalance2 = accounts
+//   .flatMap(acc => acc.movements) // with one step in the nested array
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance2);
+
+// Sorting Arrays
+
+// Sorting Arrays
+
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
