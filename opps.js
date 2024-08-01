ad17b97d6240723edbc9605e4f153ee0ddac5e26
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-08-01 20:08:02 +0530

    "Added new code for object-oriented programming, prototypes, and prototypal inheritance, including a coding challenge for a Car constructor function."

--- opps.js
+++ opps.js
@@ -13,6 +13,7 @@ document.querySelector('#app').innerHTML = `
 `;
 
 ('use strict');
+
 // Object oriented Programming
 // Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
@@ -46,7 +47,7 @@ console.log(jonas instanceof Person);
 // };
 // Person.hey();
 
-// Prototypes
+// Prototypes--------------------IMP
 console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
@@ -71,3 +72,89 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
+
+/*
+The Complete JavaScript Course 27
+Object Oriented Programming (OOP)
+Coding Challenge #1
+Your tasks:
+1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+'speed' property. The 'speed' property is the current speed of the car in
+km/h
+2. Implement an 'accelerate' method that will increase the car's speed by 10,
+and log the new speed to the console
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+the new speed to the console
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+*/
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+// console.log(bmw.make, bmw.speed);
+// console.log(mercedes.make, mercedes.speed);
+
+// Car.prototype.accelerate = function () {
+//   console.log(this.speed += 10);
+// };
+// bmw.accelerate();
+// mercedes.accelerate();
+
+// Car.prototype.brake = function () {
+//   console.log(this.speed / 5);
+// };
+// bmw.brake();
+// mercedes.brake();
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+bmw.brake();
