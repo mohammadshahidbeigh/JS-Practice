501941bc61937aaa2bb671a0ab32f1be757539dc
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-08-01 14:26:44 +0530

    "Added Object Oriented Programming examples to opps.js, including constructor functions, prototypes, and instance properties."

--- opps.js
+++ opps.js
@@ -11,3 +11,63 @@ document.querySelector('#app').innerHTML = `
     <h3>This development environment contains JavaScript problem-solving exercises for practice.</h3>
   </div>
 `;
+
+('use strict');
+// Object oriented Programming
+// Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  // Traditionally this is a class/blueprint
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never use a method like this inside a Constructor function!
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991); // and these are the instances!
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(jonas instanceof Person);
+
+// Person.hey = function () {
+//   console.log('Hey there 👋');
+//   console.log(this);
+// };
+// Person.hey();
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototyeOfLinkedObjects
+
+// Adding Properties to the constructor function using a prototype
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
